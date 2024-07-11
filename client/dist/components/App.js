928a0501d7ba1228222cf77d476b02a9f3b4b38d
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-07-11 19:35:43 -0400

    dist

---
+++ client/dist/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Heading, Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p="4">
+      <Heading size="lg" mb="4">Welcome to Chakra UI</Heading>
+      <Button colorScheme="blue">Click me</Button>
+    </Box>
+  );
+}
+
+export default App;
