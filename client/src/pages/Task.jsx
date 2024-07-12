05a417752a1ecb76507ab18659a76d4f27bb3b30
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-11 20:59:34 -0400

    addtl task page edits

--- client/src/pages/Task.jsx
+++ client/src/pages/Task.jsx
@@ -1,26 +1,32 @@
+// Component renders a box containing a title, a list of tasks, and an input field with a button to add new tasks
+
 import React, { useState } from 'react';
+// Chakra UI components for styling, functionality
 import { Box, Heading, Text, Input, Button, VStack, HStack, Checkbox } from '@chakra-ui/react';
 
 export default function Task() {
+  // Define state to manage tasks (array of tasks), new task input (string for new task input)
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
+  // Function to add a new task to 'tasks' state array when "Add Task" button is clicked; Input cannot be empty
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
       setTasks([...tasks, { text: newTask, completed: false }]);
       setNewTask('');
     }
   };
-
+    // Indicate task completion status; Function to toggle completion status of a task when corresponding checkbox is clicked
     const handleToggleTask = (index) => {
     const updatedTasks = tasks.map((task, i) =>
-    i === index ? { ...task, completed: !task.completed } : task
+      i === index ? { ...task, completed: !task.completed } : task
     );
+    // Update tasks w/ updatedTasks array
     setTasks(updatedTasks);
   };
 
   return (
-    <Box p={4}>
+    <Box p={4} bg="purple.800" color="white" borderRadius="md" boxShadow="lg">
       <Heading as="h1" size="xl" mb={4}>
         Planisphere
       </Heading>
