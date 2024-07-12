992bb60faef66d57dad6e4c2ff58e16bf4fbed31
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-11 20:02:45 -0400

    home and error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,16 +1,36 @@
-import { useRouteError } from 'react-router-dom';
+import React from 'react';
+import { Box, Heading, Text, Button, VStack } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+export default function Error() {
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.push('/');
+  };
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    height="100vh"
+    bg="gray.100"
+    p={4}
+    >
+
+      <VStack spacing={4} textAlign="center">
+        <Heading as ="h1" size="2xl" color="red.500">
+          Houston, We Have a Problem!
+        </Heading>
+        <Text fontSize="xl" color="gray.700">
+          It looks like this page is lost in space. Please try again later or navigate to a safe orbit.
+        </Text>
+        <Button colorScheme="teal" onClick={handleGoBack}>
+          Return to Mission Control
+        </Button>
+      </VStack>
+    </Box>
   );
 }
+
