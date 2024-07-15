97f86ba876079b5919957994e7d8ab82ffb1b19a
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-15 18:46:31 -0400

    start login/register page

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,6 @@
+import React { useState } from 'react';
+import { Box, Button, FormControl, FormLabel, Input, Stack, Heading, Text, Alert, AlertIcon } from '@chakra-ui/react';
+
+function LoginPage () {
+    
+}
